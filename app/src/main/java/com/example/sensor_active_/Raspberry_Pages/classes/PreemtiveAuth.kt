f40f62d5619b6a565add38db150feec1b1af71e4
RuffBen <33577643+RuffBen@users.noreply.github.com>
2021-04-27 21:08:53 +0200

    Optimierungen für POST Requests

---
+++ app/src/main/java/com/example/sensor_active_/Raspberry_Pages/classes/PreemtiveAuth.kt
@@ -0,0 +1,123 @@
+package com.example.sensor_active_.Raspberry_Pages.classes
+
+import android.util.Log
+import okhttp3.*
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
+var client = OkHttpClient()
+
+class PreemtiveAuth(_url: String, _extension: String, _username: String, _password: String) {
+    var url: String = _url
+    var extension: String = _extension
+    var username: String = _username
+    var password: String = _password
+
+    init {
+        //Wenn man kein vertrauenswürdiges certifikat hat: https://stackoverflow.com/questions/25509296/trusting-all-certificates-with-okhttp
+        // Create a trust manager that does not validate certificate chains
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+            }
+
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+            }
+
+            override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
+        })
+
+        // Install the all-trusting trust manager
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+        // Create an ssl socket factory with our all-trusting manager
+        val sslSocketFactory = sslContext.socketFactory
+
+
+        //Build Client
+        client = OkHttpClient.Builder()
+                .addInterceptor(
+                        BasicAuthInterceptor(url, extension, username, password))
+                .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+                .hostnameVerifier { _, _ -> true }
+                .build()
+
+
+        //Wenn man ein vertrauenswürdiges Certifikat hat:
+        /*
+        client = OkHttpClient.Builder()
+                .addInterceptor(
+                        BasicAuthInterceptor(url, extension, username, password))
+            .build()
+            */
+    }
+
+
+    fun run(): String {
+        var textResponse: String = "Server is not responding - check your IP address and try again"
+
+        var request: Request = Request.Builder()
+                .url(url + extension)
+                .build()
+
+        when (extension) {
+            "/status" -> request = status(request)
+            "/change" -> request = changeintervall(request)
+            "/sensoractive_gateway" -> request = status(request)
+
+            else -> textResponse = "Unknown extension, please contact an admin on gitHub"
+
+        }
+
+
+
+
+
+        Log.i("PreemtiveAuth-Method", request.toString())
+
+
+        try {
+            //Hier wird der finale Aufruf abgeschickt
+
+            var response: Response = client.newCall(request).execute()
+            textResponse = response.body!!.string()
+
+        } catch (e: Exception) {
+
+            Log.i("Preem-Counter: ", "04-Error")
+
+            println(e.toString())
+        }
+        Log.i("TextResponse ", "Text: "+  textResponse)
+        return textResponse
+
+    }
+
+
+    fun status(_request: Request): Request {
+        var request = _request
+        request = request.newBuilder()
+                .post(FormBody.Builder().build())
+                .build()
+
+        return request
+
+    }
+
+    fun changeintervall(_request: Request): Request {
+        var request = _request
+
+        val formBody: RequestBody = FormBody.Builder()
+                .add("sensor", "m5stack2")
+                .add("intervall", "22")
+                .add("intervall", "1337")
+                .build();
+        request = request.newBuilder()
+                .post(formBody)
+                .build()
+
+        return request
+
+    }
+}
--- app/src/main/java/com/example/sensor_active_/Raspberry_Pages/connectRaspberry.kt
+++ app/src/main/java/com/example/sensor_active_/Raspberry_Pages/connectRaspberry.kt
@@ -1,14 +1,15 @@
 package com.example.sensor_active_.Raspberry_Pages
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.example.sensor_active_.R
+import com.example.sensor_active_.Raspberry_Pages.classes.PreemtiveAuth
 import kotlinx.android.synthetic.main.activity_connect_raspberry.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.net.InetAddress
 
 class connectRaspberry : AppCompatActivity() {
     //HTTPS links only
@@ -16,70 +17,61 @@ class connectRaspberry : AppCompatActivity() {
     var extension = ""
     var username = ""
     var password = ""
-    var POST_message = ""
+    val PORT = ":8888"
     val watingMessage = "waiting for results..."
     var textViewContent:String = ""
-    var recievedIPAddress:String = "non"
+    var recievedIPAddress = "non"
     var recievedHostName:String = "non"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect_raspberry)
         recievedIPAddress = intent.getStringExtra("ip_Address")
-
-        if(recievedIPAddress.contains("///")){
-            var delimiter = "///"
+         val delimiter = "///"
             recievedHostName = recievedIPAddress.split(delimiter)[0]
             recievedIPAddress = recievedIPAddress.split(delimiter)[1]
-            recievedIPAddress = "https://" + recievedIPAddress + ":8888"
+            recievedIPAddress = "https://" + recievedIPAddress + PORT
             Log.i("SPLITTED STRING", recievedIPAddress)
 
-        }
+
         displayIpAddress.text = recievedHostName + "\n" + recievedIPAddress
 
     }
+
+
     fun getInputs() {
         text_view_result.text = watingMessage
-
         url = recievedIPAddress
         username = ip_username.text.toString()
         password = ip_password.text.toString()
     }
 
+
+
     fun getStatus(view: View) {
         extension = "/status"
         getInputs()
         // Run on other Thread
-        //start Process
         GlobalScope.launch {
-            textViewContent= PreemtiveAuth(url, extension, username, password, false).run()
+            textViewContent= PreemtiveAuth(url, extension, username, password).run()
             runOnUiThread{
                 text_view_result.text = textViewContent
             }
         }
 
     }
+
+
     fun sendPost(view: View) {
-        Log.i("POSTMessage", "HELLO POST")
-        extension = "/changeintervall"
+        extension = "/change" //Option -> KEY hier
         getInputs()
+        // Run on other Thread
         GlobalScope.launch {
-            textViewContent= PreemtiveAuth(url, extension, username, password, true).run()
+            textViewContent= PreemtiveAuth(url, extension, username, password).run()
             runOnUiThread{
                 text_view_result.text = textViewContent
             }
         }
 
     }
-    fun withLink(view: View) {
-        extension = postMessage.text.toString()
-        Log.i("POSTMessage", extension)
 
-        getInputs()
-        GlobalScope.launch {
-            textViewContent= PreemtiveAuth(url, extension, username, password, false).run()
-            runOnUiThread{
-                text_view_result.text = textViewContent
-            }
-        }
-    }
 }
